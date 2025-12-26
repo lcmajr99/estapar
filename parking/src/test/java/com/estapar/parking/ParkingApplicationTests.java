0e7951231206fec416cb46399033242ec0fdacb9
Luis Carlos Jr <lcmajr.cc@gmail.com>
2025-12-26 02:18:08 -0300

    luis - test: disable default Spring Boot context test

--- parking/src/test/java/com/estapar/parking/ParkingApplicationTests.java
+++ parking/src/test/java/com/estapar/parking/ParkingApplicationTests.java
@@ -1,13 +1,17 @@
 package com.estapar.parking;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/**
+ * Teste de contexto desabilitado temporariamente.
+ * Testes reais serão implementados na etapa de implementação.
+ */
+@Disabled("Disabled until tests are implemented")
 class ParkingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// intentionally disabled
 	}
-
 }
